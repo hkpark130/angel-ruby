ac917673a2d73fabf64163d182d887c0d2b22389
hkpark130 <hkpark130@naver.com>
2023-05-17 03:28:50 +0900

    migrate 실행

--- db/migrate/20230516182605_create_posts.rb
+++ db/migrate/20230516182605_create_posts.rb
@@ -3,6 +3,8 @@ class CreatePosts < ActiveRecord::Migration[5.2]
     create_table :posts do |t|
 
       t.timestamps
+      t.text :title
+      t.text :body
     end
   end
 end
--- db/migrate/20230516182628_create_users.rb
+++ db/migrate/20230516182628_create_users.rb
@@ -3,6 +3,11 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
 
       t.timestamps
+      t.string :firstName
+      t.string :lastName
+      t.string :email
+      t.string :password
+      t.string :country
     end
   end
 end
