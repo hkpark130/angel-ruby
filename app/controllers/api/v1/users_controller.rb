1d846df50d4f8202a5750602bded6437ac0c7301
hkpark130 <hkpark130@naver.com>
2023-05-17 03:31:17 +0900

    route 추가, signup 추가

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::UsersController < ApplicationController
+    protect_from_forgery
+    def signup
+        user = User.new
+        user.firstName = params[:firstName]
+        user.lastName = params[:lastName]
+        user.email = params[:email]
+        user.password = params[:password]
+        user.country = params[:country]
+        user.save
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      post 'users/signup', to: 'users#signup'
+      post 'auth/signin', to: 'auth#signin'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
