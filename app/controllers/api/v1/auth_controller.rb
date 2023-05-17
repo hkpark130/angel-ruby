4eaf9183fc239454715cdc3c8bd4b4ee3e6586bf
hkpark130 <hkpark130@naver.com>
2023-05-18 03:27:50 +0900

    signin jwt token 생성

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -4,12 +4,13 @@ class Api::V1::AuthController < ApplicationController
         param = params[:auth]
         user = User.find_by(email: param['email'])
         if user && user.authenticate(param['password'])
+            token = generate_token(user)
             response = {
                 data:{
                     id: user.id,
                     type: "users",
                     attributes: {
-                        token: "HEADER.PAYLOAD.SIGNATURE",
+                        token: token,
                         email: param['email'],
                         name: user.firstName + " " + user.lastName,
                         country: user.country,
@@ -21,7 +22,11 @@ class Api::V1::AuthController < ApplicationController
             render json: response, status: :ok
         else
             render json: {error: "Invalid email or password"}, status: :unauthorized
-        end
-        
+        end 
+    end
+
+    def generate_token(user)
+        payload = { user_id: user.id }
+        JWT.encode(payload, Rails.application.secrets.secret_key_base)
     end
 end
